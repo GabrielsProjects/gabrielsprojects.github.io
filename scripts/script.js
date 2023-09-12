14e2d4fab50ef9d2c8cd08018ba035e67d8a1f03
Gabriel Davis <103909311+GabrielsProjects@users.noreply.github.com>
2023-09-12 17:54:20 -0400

    Full Overhaul 2.0

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get references to the list items by their IDs
-    var homeListItem = document.getElementById("nav-home");
-    var projectsListItem = document.getElementById("nav-projects");
-    var linksListItem = document.getElementById("nav-links");
-    var fixedIcon = document.querySelector(".fixed-icon");
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+            
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
 
-    function changeColorToGrey(item) {
-        var link = item.querySelector('a');
-        link.classList.add('menu-item-clicked');
-        link.style.color = '#08f1cc';
-        setTimeout(function() {
-            link.style.color = '';
-            link.classList.remove('menu-item-clicked');
-        }, 200); // Reset to white after 0.3 seconds
-    }
-
-    // Add an onclick event handler to each list item
-    homeListItem.onclick = function(e) {
-        e.preventDefault(); // Prevent default link behavior
-        changeColorToGrey(this);
+    const aboutLink = document.querySelector('#nav-about a');
+    aboutLink.addEventListener("click", function(e) {
+        e.preventDefault();
+        
+        // Scroll to the top of the page
         window.scrollTo({
             top: 0,
             behavior: "smooth"
-        })
-    };
-
-    projectsListItem.onclick = function(e) {
-    e.preventDefault(); // Prevent default link behavior
-        changeColorToGrey(this);
-        scrollToSection("projects");
-    };
-
-    linksListItem.onclick = function(e) {
-      e.preventDefault(); // Prevent default link behavior
-        changeColorToGrey(this);
-        scrollToSection("links");
-    };
-
-    // Function to smoothly scroll to a section by its ID
-    function scrollToSection(sectionId) {
-        var section = document.getElementById(sectionId);
-        if (section) {
-            window.scrollTo({
-                behavior: 'smooth',
-                top: section.offsetTop
-            });
-        }
-    }
-
-    // Add an opacity animation to the fixed icon
-    fixedIcon.style.transition = "opacity 0.5s ease";
-
-    // Check scroll position to change the opacity of the arrow
-    window.addEventListener("scroll", function() {
-        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        fixedIcon.style.opacity = "0"; // Fade out the arrow
-    } else {
-        fixedIcon.style.opacity = "1"; // Fade in the arrow
-    }
+        });
     });
 
-    // Get the "View Resume" button by its ID
-    var resumeButton = document.getElementById("resume");
+    const arrow = document.getElementById("bottom-arrow");
+    arrow.addEventListener("click", function(e) {
+        e.preventDefault();
 
-    // Add a click event handler to the button
-    resumeButton.onclick = function() {
-        // Get the PDF document URL from the data attribute
-        var pdfUrl = this.getAttribute("data-pdf-url");
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth",
+        })
+    });
 
-        // Open the PDF document in a new tab
-        if (pdfUrl) {
-            window.open(pdfUrl, '_blank');
+    document.addEventListener('scroll', function(e) {
+        let documentHeight = document.body.scrollHeight;
+        let currentScroll = window.scrollY + window.innerHeight;
+
+        let modifier = 20; 
+        if(currentScroll + modifier > documentHeight) {
+            arrow.style.opacity = 0;
+        } else {
+            arrow.style.opacity = .4;
         }
-    };
+    })
 });
